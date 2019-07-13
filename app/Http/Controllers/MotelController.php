a4219496c24a5d1654d98c1a06add75adb38731e
kimxuanhong <kimxuanhong>
2019-07-13 15:29:08 +0700

    first commit

---
+++ app/Http/Controllers/MotelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Motel;
+use Illuminate\Http\Request;
+
+class MotelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Motel  $motel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Motel $motel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Motel  $motel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Motel $motel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Motel  $motel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Motel $motel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Motel  $motel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Motel $motel)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/StaticDataController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\StaticData;
+use Illuminate\Http\Request;
+
+class StaticDataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\StaticData  $staticData
+     * @return \Illuminate\Http\Response
+     */
+    public function show(StaticData $staticData)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\StaticData  $staticData
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(StaticData $staticData)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\StaticData  $staticData
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, StaticData $staticData)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\StaticData  $staticData
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(StaticData $staticData)
+    {
+        //
+    }
+}
